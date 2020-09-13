cd79746543734f16df824403a46ed0aecf299d2d
traininguniverse <treninguniverse@gmail.com>
2020-09-13 17:12:09 +0200

    add CRUD with postman collection

---
+++ tutorialYT/JavaBrains/Spring Boot Quick Start/partI/src/main/java/net/traininguniverse/springstarter/Starter.java
@@ -0,0 +1,13 @@
+package net.traininguniverse.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starter.class, args);
+	}
+
+}
